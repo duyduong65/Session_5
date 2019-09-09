b2ebbf2b3d95847d0a9aa3c35b155eabfd2046d0
duyduong65 <tg.bluesky66@gmail.com>
2019-09-09 16:51:27 +0700

    sửa biến =))

--- L-Dictionary/Main_Dictionary.js
+++ L-Dictionary/Main_Dictionary.js
@@ -1,13 +1,13 @@
 function Translate() {
     let search = document.getElementById('searchString').value;
     let dictionary = ["red", "yellow", "blue", "green"];
-    let result = ["đỏ", "vàng", "xanh lục", "xanh lá"];
+    let thenTranslate = ["đỏ", "vàng", "xanh lục", "xanh lá"];
 
 
     for (let i = 0; i < dictionary.length; i++) {
         if (dictionary[i] === search) {
-            document.getElementById('showString').value = result[i];
-        } else if(result[i] === search){
+            document.getElementById('showString').value = thenTranslate[i];
+        } else if(thenTranslate[i] === search){
             document.getElementById('showString').value = dictionary[i];
         }
     }
