dceb254ca51e6e94d4c476b0956b397049115c89
duyduong65 <tg.bluesky66@gmail.com>
2019-09-10 11:18:43 +0700

    add ex_4

--- Luyện tập mảng _1/Main_example.js
+++ Luyện tập mảng _1/Main_example.js
@@ -18,5 +18,22 @@ function ex2() {
             length = i;
         }
     }
-    document.write("max number: " + maxNumber + " "+ "length: "+ length);
+    document.write("max number: " + maxNumber + " " + "length: " + length);
+}
+
+function ex3() {
+    let array = [1, 3, 4, 22, 54, 23, 2, 77, 9, 10];
+    let maxNumber = array[0];
+    let trungbinh = 0;
+    for (let i = 0; i < array.length; i++) {
+        if (maxNumber < array[i]) {
+            maxNumber = array[i];
+        }
+        trungbinh = trungbinh + array[i]
+    }
+    trungbinh = trungbinh / array.length;
+    document.write("số lớn nhất là: " + maxNumber + " " + "trung bình của mảng là: " + trungbinh);
+}
+function ex4() {
+    
 }
